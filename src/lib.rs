2ef67b97a265ca18bcf1c7345748d64a7179ea1c
Pablo Deymonnaz <deymonnaz@gmail.com>
2020-10-11 01:27:16 -0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
 use actix_web::{http::StatusCode, Responder};
+use anyhow::{anyhow, Result};
+use async_std::prelude::*;
+use futures::{StreamExt, TryStreamExt};
+use uuid::Uuid;
+
+pub async fn receive_multipart_file(mut body: actix_multipart::Multipart) -> Result<String> {
+    let mut filepath_dest = String::new();
+    // recibir el archivo
+    // iterate over multipart stream
+    while let Ok(Some(mut field)) = body.try_next().await {
+        let content_type = field
+            .content_disposition()
+            .ok_or_else(|| anyhow!("Error receiving file"))?;
+        let filename = content_type
+            .get_filename()
+            .ok_or_else(|| anyhow!("Error receiving file"))?;
+        let file_uuid = Uuid::new_v4();
+        let filepath = format!("./{}{}", file_uuid, sanitize_filename::sanitize(&filename));
+        filepath_dest = filepath.clone();
+        let mut f = async_std::fs::File::create(filepath).await?;
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            f.write_all(&data).await?;
+        }
+    }
+    Ok(filepath_dest)
+}
 
 pub async fn pass_post_to_server(url: &str, req: &impl serde::Serialize) -> impl Responder {
     let client = reqwest::Client::new();
