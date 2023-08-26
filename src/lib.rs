7d042afe2c572a9284776d81c768667d9a4a31be
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-08-26 13:27:38 -0300

    upgrade local

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use actix_web::{web, HttpResponse};
 use anyhow::{anyhow, Result};
-use async_std::prelude::*;
 use error_utils::msghttp::MsgHttp;
 use futures::{StreamExt, TryStreamExt};
 use path_utils::convert_filename_extension_to_lowercase;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt; // for write_all()
 use uuid::Uuid;
 
 pub async fn receive_multipart_file(mut body: actix_multipart::Multipart) -> Result<String> {
@@ -18,13 +19,13 @@ pub async fn receive_multipart_file(mut body: actix_multipart::Multipart) -> Res
         let filepath_uuid = format!(
             "./{}{}",
             Uuid::new_v4(),
-            sanitize_filename::sanitize(&filename)
+            sanitize_filename::sanitize(filename)
         );
 
         let filepath = convert_filename_extension_to_lowercase(&filepath_uuid)
             .ok_or_else(|| anyhow!("Filename error"))?;
         filepath_dest = filepath.clone();
-        let mut f = async_std::fs::File::create(filepath).await?;
+        let mut f = File::create(filepath).await?;
 
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
@@ -66,7 +67,7 @@ pub fn send_file_content_and_delete_file(
 ) -> Result<HttpResponse, MsgHttp> {
     // leer el contenido del archivo temporal a un buffer de bytes
     let file_content =
-        web::Bytes::from(std::fs::read(&filename).map_err(|e| MsgHttp::new(e.to_string(), 500))?);
+        web::Bytes::from(std::fs::read(filename).map_err(|e| MsgHttp::new(e.to_string(), 500))?);
     // eliminar el archivo temporal
     std::fs::remove_file(filename).map_err(|e| MsgHttp::new(e.to_string(), 500))?;
 
